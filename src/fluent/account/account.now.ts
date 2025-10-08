2391665cf8ce2af29674585c6c543a1a4de69240
System Administrator <admin@example.com>
2025-10-08 13:50:15 -0700

    refactoring table

---
+++ src/fluent/account/account.now.ts
@@ -0,0 +1,63 @@
+import { ChoiceColumn, DecimalColumn, ReferenceColumn, StringColumn, Table } from '@servicenow/sdk/core'
+import '@servicenow/sdk/global'
+
+export const x_842748_budgetnow_account = Table({
+    name: 'x_842748_budgetnow_account',
+    label: 'Account',
+    extensible: true,
+    schema: {
+        name: StringColumn({
+            active: true,
+            label: 'Name',
+            maxLength: 40,
+            mandatory: true,
+        }),
+
+        balance: DecimalColumn({
+            active: true,
+            label: 'Balance',
+            attributes: {
+                scale: 2,
+            },
+            default: '0',
+        }),
+
+        type: ChoiceColumn({
+            active: true,
+            label: 'Type',
+            mandatory: true,
+            choices: {
+                checking: {
+                    label: 'Checking',
+                    inactive: false,
+                    sequence: 100,
+                    inactive_on_update: false,
+                },
+                savings: {
+                    label: 'Savings',
+                    inactive: false,
+                    sequence: 200,
+                    inactive_on_update: false,
+                },
+                credit: {
+                    label: 'Credit',
+                    inactive: false,
+                    sequence: 300,
+                    inactive_on_update: false,
+                },
+            },
+        }),
+
+        account_owner: ReferenceColumn({
+            label: 'Owner',
+            mandatory: true,
+            referenceTable: 'sys_user',
+            attributes: {
+                encode_utf8: false,
+            },
+            reference_qual: 'active=true^EQ',
+        }),
+    },
+    display: 'name',
+    index: [{ name: 'index', element: 'account_owner', unique: false }],
+})
