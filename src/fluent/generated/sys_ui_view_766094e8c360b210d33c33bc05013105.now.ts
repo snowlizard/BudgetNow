3c44303d2bc4b07e97cd3f4025ccba9134be7665
System Administrator <admin@example.com>
2025-10-08 21:07:28 -0700

    form view add

---
+++ src/fluent/generated/sys_ui_view_766094e8c360b210d33c33bc05013105.now.ts
@@ -0,0 +1,12 @@
+import { Record } from '@servicenow/sdk/core'
+
+export default Record({
+    $id: '766094e8c360b210d33c33bc05013105',
+    table: 'sys_ui_view',
+    data: {
+        hidden: false,
+        name: 'Withdrawal',
+        sys_name: 'Withdrawal',
+        title: 'Withdrawal',
+    },
+})
