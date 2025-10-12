c8c0e746aa4c739fde9e06de421f73f5c7c56090
System Administrator <admin@example.com>
2025-10-12 09:17:16 -0700

    adding dashboard

---
+++ src/fluent/generated/par_dashboard_cdfc4a6983607210066a5d10feaad327.now.ts
@@ -0,0 +1,15 @@
+import { Record } from '@servicenow/sdk/core'
+
+export default Record({
+    $id: 'cdfc4a6983607210066a5d10feaad327',
+    table: 'par_dashboard',
+    data: {
+        active: true,
+        certified: false,
+        grid: 48,
+        name: 'My finances',
+        sys_domain: 'global',
+        sys_domain_path: '/',
+        sys_name: 'My finances',
+    },
+})
